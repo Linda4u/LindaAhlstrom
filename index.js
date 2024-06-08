b1cd1627f2bff0f81af19f83f9f95fa844ab1ad7
Linda4u <138815179+Linda4u@users.noreply.github.com>
2024-06-07 18:01:40 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,41 @@
 //     *the page it's changing
 //     *the color you want to tab to update
 // It updates the color of the tabs and the display of the tabs
+
+
+var x = document.getElementById("Breads");
+x.addEventListener("mouseover,openCity");
+x.addEventListener("click", openPage);
+
+var x = document.getElementById("Main Dishes");
+x.addEventListener("mouseover,openCity");
+x.addEventListener("click", openPage);
+
+var x = document.getElementById("Desserts");
+x.addEventListener("mouseover,openCity");
+x.addEventListener("click", openPage);
+
+var x = document.getElementById("Sides");
+x.addEventListener("mouseover,openCity");
+x.addEventListener("click", openPage);
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Get the notepad body element
+  var notepadBody = document.getElementById('notepad-body');
+
+  // Load saved notes from localStorage
+  if (localStorage.getItem('notes')) {
+    notepadBody.innerHTML = localStorage.getItem('notes');
+  }
+
+  // Save notes to localStorage whenever content changes
+  notepadBody.addEventListener('input', function () {
+    localStorage.setItem('notes', this.innerHTML);
+  });
+});
+
+
+
 function openPage(elementId, page, color) {
   //Name my variables
   var i, tabcontent, tablinks;
@@ -10,7 +45,7 @@ function openPage(elementId, page, color) {
   tabcontent = document.getElementsByClassName("tabcontent");
   // Get html elements with the class name "tablink" and set tablink variable
   tablinks = document.getElementsByClassName("tablink");
-  // loop through every element in the tabcontent variable, set the display to none
+  // loop through every element in the tabcontent variable, set the display to none 
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
@@ -24,9 +59,9 @@ function openPage(elementId, page, color) {
   page.style.backgroundColor = color;
   let dispage;
 
-  const tabname = elementId;
-  //if (tabname = "defaultOpen") { dispage = 'index'; } else { dispage = tabname; }
-  tabname.innerHTML = "hi" + dispage + "!"
+  // const tabname = elementId;
+  // //if (tabname = "defaultOpen") { dispage = 'index'; } else { dispage = tabname; }
+  // tabname.innerHTML = "hi" + dispage + "!"
 
   window.location = elementId + '.html'
 
